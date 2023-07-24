aa7cbf2ac6fd226268709b4e5ea9860f13e31027
Paskinho <v.paskanov@gmail.com>
2023-07-24 21:41:17 +0300

    Some refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import { App } from "./App.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
