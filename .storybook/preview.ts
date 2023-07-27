d77862de3439dae71b0a1fe30b7a3216ba2656e0
Paskinho <v.paskanov@gmail.com>
2023-07-27 19:58:39 +0300

    Some refactoring in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 
+import '../src/styles/index.scss'
+
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
