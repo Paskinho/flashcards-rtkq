1e0ffd3acba7fc58134f5a1e8c5ac5445bd6f4d0
Paskinho <v.paskanov@gmail.com>
2023-07-27 19:35:55 +0300

    Add some files and colors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,6 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
-import "./index.css";
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
