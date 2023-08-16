33ac9937ab6cde4515f726563b07aba477ec442b
Paskinho <v.paskanov@gmail.com>
2023-08-16 12:00:16 +0300

    Add styles and new component button.tsx

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,10 +1,22 @@
 import { ComponentPropsWithoutRef } from "react";
 
+import s from "button.module.css";
+
 export type ButtonProps = {
   variant?: "primary" | "secondary" | "tertiary" | "link";
   fullWidth?: boolean;
 } & ComponentPropsWithoutRef<"button">;
 
-export const Button = () => {
-  return <div>Button</div>;
+export const Button = ({
+  variant = "primary",
+  fullWidth,
+  className,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <button
+      className={`${s[variant]} ${fullWidth ? s.fullWidth : ""} ${className}`}
+      {...rest}
+    />
+  );
 };
