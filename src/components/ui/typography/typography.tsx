0f0d04d16357c04405b46b7dfcbb3a4d2c65e08e
Paskinho <v.paskanov@gmail.com>
2023-08-17 16:17:30 +0300

    Refactoring typization in typography.tsx

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,52 +1,25 @@
-import {
-  ComponentProps,
-  ElementType,
-  JSXElementConstructor,
-  ReactNode,
-} from "react";
+import { ReactNode } from "react";
 
 import s from "./typography.module.css";
 
-export type PropsOf<TTag extends ReactTag> = TTag extends ElementType
-  ? Omit<ComponentProps<TTag>, "ref">
-  : never;
-export type ReactTag = keyof JSX.IntrinsicElements | JSXElementConstructor<any>;
-
 export type TypographyProps = {
   children: ReactNode;
-  component?: Ttag;
+  color?: "primary" | "secondary" | "inherit" | "error";
+  variant?:
+    | "large"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "body1"
+    | "body2"
+    | "subtitle1"
+    | "subtitle2"
+    | "caption"
+    | "overline"
+    | "link1"
+    | "link2"
+    | "error";
   className?: string;
-  mb?: number | string;
-  mt?: number | string;
-  mr?: number | string;
-  ml?: number | string;
-  mx?: number | string;
-  my?: number | string;
-};
-
-const createTypographyComponent = () => {
-  return ({ children, component, className, style, mr, ml, mt, mb, mx, my, ...rest }) => {
-    const Component = component || COMPONENTS[basicClassName] || 'span'
-
-    const classNames = clsx(s[basicClassName], className)
-
-    const styles = {
-      ...(mr && { marginRight: mr }),
-      ...(ml && { marginLeft: ml }),
-      ...(mt && { marginTop: mt }),
-      ...(mb && { marginBottom: mb }),
-      ...(mx && { marginRight: mx, marginLeft: mx }),
-      ...(my && { marginTop: my, marginBottom: my }),
-      ...style,
-    }
-
-    return (
-        <Component className={classNames} style={styles} {...rest}>
-          {children}
-        </Component>
-    )
-  }
-}
 };
 
 export const Typography = () => {
