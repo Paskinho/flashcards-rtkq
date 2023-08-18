ae7b89c3350969c2abf160434e9eede7e79c51ce
Paskinho <v.paskanov@gmail.com>
2023-08-18 14:31:58 +0300

    Refactoring checkbox.stories.ts

--- src/components/ui/checkbox/checkbox.stories.ts
+++ src/components/ui/checkbox/checkbox.stories.ts
@@ -9,5 +9,26 @@ const meta = {
 };satisfies Meta<typeof Checkbox>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
+type Story = StoryObj<typeof meta>
+export const Uncontrolled: Story = {
+    args: {
+        label: 'Click here',
+        disabled: false,
+    },
+}
+
+export const Controlled: Story = {
+    render: args => {
+        const [checked, setChecked] = useState(false)
+
+        return (
+            <Checkbox
+                {...args}
+        label="Click here"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+        />
+    )
+    },
+}
