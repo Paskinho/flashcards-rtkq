e1eb680c8b6609f551810870f7e56679795dd258
Paskinho <v.paskanov@gmail.com>
2023-08-18 12:24:41 +0300

    Add Props and some refactoring to Checkbox component

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,8 +1,35 @@
+import { ReactNode } from "react";
+
 export type Checkbox = {};
 
-export const Checkbox = () => {
+export type CheckboxProps = {
+  className?: string;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
+  disabled?: boolean;
+  required?: boolean;
+  label?: string;
+  id?: string;
+  position?: "left";
+};
+
+export const Checkbox = ({
+  className,
+  checked,
+  onChange,
+  disabled,
+  required,
+  label,
+  id,
+  position,
+}) => {
   return (
-    <Flex>
+
+      const classNames = {
+
+      }
+
+      <Flex>
       <Text size="2">
         <label>
           <Checkbox mr="1" defaultChecked /> Agree to Terms and Conditions
