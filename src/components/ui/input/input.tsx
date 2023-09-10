1ed232cc10e2b87683ad15daacffa1e98ef8c20f
Paskinho <v.paskanov@gmail.com>
2023-09-10 13:39:42 +0300

    Add type for input

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,1 +1,20 @@
-export const Input = () => {};
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+
+export type InputProps = {
+  label?: string;
+  disabled?: boolean;
+  inputTextClassName?: string;
+  type?: string;
+  error?: string;
+  onClickClearInput?: () => void;
+  searchInput?: boolean;
+  width?: string;
+} & ComponentPropsWithoutRef<"input">;
+
+export const Input = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
