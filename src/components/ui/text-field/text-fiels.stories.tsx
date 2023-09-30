a7d5b9c148d8743b6764e5e7ba6c89ac4e2b40f2
Paskinho <v.paskanov@gmail.com>
2023-09-30 13:53:06 +0300

    Create stories for text-field

---
+++ src/components/ui/text-field/text-fiels.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/blocks";
+import { TextField } from "src/components/ui/text-field/text-field";
+
+export default {};
+const meta = {
+  title: "Components/TextField",
+  component: TextField,
+  tags: ["autodocs"],
+} satisfies Meta<typeof TextField>;
