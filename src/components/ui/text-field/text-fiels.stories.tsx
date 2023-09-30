e095fae52d15df13d7d97785dd2c23b623d6ec4b
Paskinho <v.paskanov@gmail.com>
2023-09-30 13:55:07 +0300

    Complete stories for text-field

--- src/components/ui/text-field/text-fiels.stories.tsx
+++ src/components/ui/text-field/text-fiels.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta } from "@storybook/blocks";
+import { StoryObj } from "@storybook/react";
 import { TextField } from "src/components/ui/text-field/text-field";
 
 export default {};
@@ -6,4 +7,33 @@ const meta = {
   title: "Components/TextField",
   component: TextField,
   tags: ["autodocs"],
-} satisfies Meta<typeof TextField>;
+};
+
+satisfies;
+Meta<typeof TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Llaceholder",
+  },
+};
+
+export const Password: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Password",
+    type: "password",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    label: "Input with error",
+    value: "Wrong value",
+    errorMessage: "Error message",
+  },
+};
