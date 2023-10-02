26beff01b8c515727e13fdbe0a6924981f5635c5
Paskinho <v.paskanov@gmail.com>
2023-10-02 18:35:45 +0300

    Some refactoring with checkbox and use useController

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useController, useForm } from "react-hook-form";
 
 import { Button } from "../../ui/button";
 import { Checkbox } from "../../ui/checkbox/checkbox";
@@ -11,18 +11,30 @@ type FormValues = {
 };
 
 export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<FormValues>();
+  const { control, handleSubmit, register } = useForm<FormValues>();
 
   const onSubmit = (data: FormValues) => {
     console.log(data);
   };
 
+  const {
+    field: { value, onChange },
+  } = useController({
+    name: "rememberMe",
+    control,
+    defaultValue: false,
+  });
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextField {...register("email")} label={"email"} />
       <TextField {...register("password")} label={"password"} />
-      {/*<Checkbox {...register("rememberMe")} label={"remember me"} /> ОТРИСОВАТЬ!!!*/}
-
+      <Checkbox
+        onCheckedChange={onChange}
+        checked={value}
+        label={"remember me"}
+      />{" "}
+      ОТРИСОВАТЬ!!!
       <Button type="submit">Submit</Button>
     </form>
   );
