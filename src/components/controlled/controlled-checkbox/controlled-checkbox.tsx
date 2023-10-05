1bdb8d393bed3ac2a6296ddc4a05b435c49cb9bf
Paskinho <v.paskanov@gmail.com>
2023-10-05 15:32:45 +0300

    Add useController to controlled-checkbox.tsx

--- src/components/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/components/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -1,1 +1,19 @@
-export const ControlledCheckbox = () => {};
+import { useController } from "react-hook-form";
+import { Checkbox, CheckboxProps } from "src/components/ui/checkbox/checkbox";
+
+export type ControlledCheckboxProps = { control: any } & CheckboxProps;
+
+export const ControlledCheckbox = ({
+  control,
+  ...rest
+}: ControlledCheckboxProps) => {
+  const {
+    field: { value, onChange },
+  } = useController({
+    name: "rememberMe",
+    control,
+    defaultValue: false,
+  });
+
+  return <Checkbox {...rest} />;
+};
