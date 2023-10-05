5cccf4df73e778af21a5edd9949b73d062a9f1a2
Paskinho <v.paskanov@gmail.com>
2023-10-05 11:23:41 +0300

    Refactoring in login-form.tsx

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -7,9 +7,9 @@ import { Checkbox } from "../../ui/checkbox/checkbox";
 import { TextField } from "../../ui/text-field";
 
 const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(3),
-  rememberMe: z.boolean().optional().default(false),
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(3, "Password must be at least 3 characters"),
+  rememberMe: z.literal(true, { invalid_type_error: "Please checkbox" }),
 });
 
 type FormValues = z.infer<typeof loginSchema>;
