64704d6bb452bb8532792ace79cf1a3fcb557507
Paskinho <v.paskanov@gmail.com>
2023-10-05 16:42:03 +0300

    Refactoring type in controlled-checkbox.tsx

--- src/components/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/components/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -1,11 +1,18 @@
-import { useController, ControllerProps, FieldValues } from "react-hook-form";
+import {
+  useController,
+  UseControllerProps,
+  FieldValues,
+} from "react-hook-form";
 
 import { Checkbox, CheckboxProps } from "../../ui/checkbox/checkbox";
 
-export type ControlledCheckboxProps<T extends FieldValues> =
-  ControllerProps<T> & Omit<CheckboxProps, "checked" | "onValueChange">;
+export type ControlledCheckboxProps<T extends FieldValues> = Pick<
+  UseControllerProps<T>,
+  "control" | "name"
+> &
+  Omit<CheckboxProps, "checked" | "onValueChange">;
 
-export const ControlledCheckbox = ({
+export const ControlledCheckbox = <T extends FieldValues>({
   control,
   name,
   ...rest
