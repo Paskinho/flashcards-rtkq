08dd4470b62c5ac865c1bd099a15be421f09e891
Paskinho <v.paskanov@gmail.com>
2023-10-05 15:28:41 +0300

    Create controlled-checkbox.tsx

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -9,7 +9,11 @@ import { TextField } from "../../ui/text-field";
 const loginSchema = z.object({
   email: z.string().email("Please enter a valid email"),
   password: z.string().min(3, "Password must be at least 3 characters"),
-  rememberMe: z.literal(true, { invalid_type_error: "Please checkbox" }),
+  rememberMe: z.literal(true, {
+    errorMap: () => ({
+      message: "Please check tre box",
+    }),
+  }),
 });
 
 type FormValues = z.infer<typeof loginSchema>;
