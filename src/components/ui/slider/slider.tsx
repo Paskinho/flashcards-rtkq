5e850762514d6751b118009a12a5454d15dcde1e
Paskinho <v.paskanov@gmail.com>
2023-10-18 19:22:41 +0300

    Finish slider component

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -5,6 +5,22 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 const Slider = forwardRef<
   ElementRef<typeof SliderPrimitive.Root>,
   ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(() => {
-  return;
-});
+>(({ className, ...props }, ref) => (
+  <div className={s.container}>
+    <span>{props?.value?.[0]}</span>
+    <SliderPrimitive.Root
+      ref={ref}
+      className={clsx(s.root, className)}
+      {...props}
+    >
+      <SliderPrimitive.Track className={s.track}>
+        <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className={s.thumb} />
+      <SliderPrimitive.Thumb className={s.thumb} />
+    </SliderPrimitive.Root>
+    <span>{props?.value?.[1]}</span>
+  </div>
+));
+
+Slider.displayName = SliderPrimitive.Root.displayName;
