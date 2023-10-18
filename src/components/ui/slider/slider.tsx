c9ee8717be602e2c0eb3da2a8333f82a9353292f
Paskinho <v.paskanov@gmail.com>
2023-10-18 19:20:01 +0300

    Some refactoring slider

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,3 +1,10 @@
-const Slider = () => {
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+const Slider = forwardRef<
+  ElementRef<typeof SliderPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(() => {
   return;
-};
+});
