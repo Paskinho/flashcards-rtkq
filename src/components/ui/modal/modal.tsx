fefe8196d0e9747e85ad4b13f0aca59560aed4c0
Paskinho <v.paskanov@gmail.com>
2023-10-22 13:41:24 +0300

    Some refactoring modal component

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -10,4 +10,16 @@ export type ModalProps = {
   size?: ModalSize;
 } & ComponentProps<"div">;
 
+const getSizeClassName = (size: ModalSize) => {
+  if (size === "sm") return s.sm;
+  if (size === "md") return s.md;
+  if (size === "lg") return s.lg;
+};
+
+const getContentClassName = (size: ModalSize, className?: string) => {
+  const sizeClassName = getSizeClassName(size);
+
+  return clsx(className, s.content, sizeClassName);
+};
+
 export const Modal = () => {};
