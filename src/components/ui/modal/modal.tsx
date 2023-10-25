94165edf6136f15e5ae47082450a687f01b50617
Paskinho <v.paskanov@gmail.com>
2023-10-25 18:33:24 +0300

    Create some functions for modal component

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -10,16 +10,39 @@ export type ModalProps = {
   size?: ModalSize;
 } & ComponentProps<"div">;
 
-const getSizeClassName = (size: ModalSize) => {
-  if (size === "sm") return s.sm;
-  if (size === "md") return s.md;
-  if (size === "lg") return s.lg;
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    x: "-50%",
+    opacity: 0,
+  },
+  visible: {
+    y: "-50%",
+    x: "-50%",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
 };
 
 const getContentClassName = (size: ModalSize, className?: string) => {
   const sizeClassName = getSizeClassName(size);
 
   return clsx(className, s.content, sizeClassName);
+};
+
+const getSizeClassName = (size: ModalSize) => {
+  if (size === "sm") return s.sm;
+  if (size === "md") return s.md;
+  if (size === "lg") return s.lg;
 };
 
 export const Modal = () => {};
