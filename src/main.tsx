54a21b4aea090b0ef6182599d9dfc24c2e28f773
Paskinho <v.paskanov@gmail.com>
2023-11-19 17:08:14 +0300

    Some refactoring code with route and more

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,27 @@ import "./styles/index.scss";
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
+import { LoginForm } from "src/components/auth/login-form/login-form";
 
 import { App } from "./App.tsx";
+
+const router = createBrowserRouter([
+  {
+    // path: '/',
+    // element: ,
+    // children: [
+    //
+    // ]
+
+    path: "/",
+    element: <div>Hello my friend!</div>,
+  },
+  {
+    path: "login",
+    element: <LoginForm />,
+  },
+]);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
