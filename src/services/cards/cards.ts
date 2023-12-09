4c9337619771198c9d4f80ba78e0e71d92204688
Paskinho <v.paskanov@gmail.com>
2023-12-09 14:17:02 +0300

    Add types for services cards

--- src/services/cards/cards.ts
+++ src/services/cards/cards.ts
@@ -1,3 +1,15 @@
-import { createApi } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-export const cardsApi = createApi({});
+export const cardsApi = createApi({
+  reducerPath: "cardsApi",
+  baseQuery: fetchBaseQuery({
+    baseURL: import.meta.env.VITE_BASE_API_URL,
+    credentials: "include",
+  }),
+  tagtypes: ["Cards"],
+  endpoints: (builder) => ({
+    getCards: builder.query<>({
+      query: ({ deckId, ...params }) => {},
+    }),
+  }),
+});
