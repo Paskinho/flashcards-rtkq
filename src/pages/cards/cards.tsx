d97311cd023b548df4c2694eb7f4ddaefcb0b0c0
Paskinho <v.paskanov@gmail.com>
2023-12-10 15:36:45 +0300

    Some refactoring card component

--- src/pages/cards/cards.tsx
+++ src/pages/cards/cards.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 import { useParams } from "react-router-dom";
 import { z } from "zod";
+
+import { Sort } from "../../../src/components/ui/table";
+import { useDeleteCardMutation } from "../../../src/services/cards/cards";
 
 const newDeckSchema = z.object({
   question: z.string().min(3).max(500),
@@ -8,7 +13,11 @@ const newDeckSchema = z.object({
 
 export const Cards = () => {
   const { deckId } = useParams<{ deckId: string }>();
-  const [deleteCard] = useDeLeteCardMutation();
+  const [deleteCard] = useDeleteCardMutation();
+  const [sort, setSort] = useState<Sort>({
+    key: "updated",
+    direction: "asc",
+  });
 
   return <div>Cards</div>;
 };
