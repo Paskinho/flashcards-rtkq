f0f31ca43836b47e47f93f09420d1e0df282b806
Paskinho <v.paskanov@gmail.com>
2023-12-10 16:28:51 +0300

    Add some types in common/types

--- src/services/common/types/index.ts
+++ src/services/common/types/index.ts
@@ -9,3 +9,15 @@ export type Paginated<T> = {
   pagination: Pagination;
   items: T[];
 };
+
+export type Sort = {
+  key: string;
+  direction: "asc" | "desc";
+} | null;
+
+export type PaginatedArgs<T extends Record<string, any> = {}> = {
+  currentPage?: number;
+  itemsPerPage?: number;
+  pageSize?: number;
+  orderBy?: string | null;
+} & T;
