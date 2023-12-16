48b9c3b63c14ec1fc918d01b188f78a3982ce379
Paskinho <v.paskanov@gmail.com>
2023-12-16 17:33:21 +0300

    Some refactoring in profile

--- src/components/profile/personal-infromation/personal-information.tsx
+++ src/components/profile/personal-infromation/personal-information.tsx
@@ -1,6 +1,8 @@
 import { Card } from "../../../../src/components/ui/card";
 import { Typography } from "../../../../src/components/ui/typography";
 
+import s from "./personal-information.module.scss";
+
 type PersonalInformationProps = {
   email: string;
   avatar: string;
@@ -19,10 +21,18 @@ export const PersonalInformation = ({
   onNameChange,
 }: PersonalInformationProps) => {
   return (
-    <Card>
+    <Card className={s.card}>
       <Typography variant={"large"}>Personal Information</Typography>
-      <Typography variant={"large"}>Personal Information</Typography>
-      <Typography variant={"large"}>Personal Information</Typography>
+      <div>
+        <div>
+          <img
+            alt={"avatar"}
+            src={
+              "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
+            }
+          />
+        </div>
+      </div>
     </Card>
   );
 };
