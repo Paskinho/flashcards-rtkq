5931fbd9a19e41ce34158a04d93b9fbc1e43cc8c
Paskinho <v.paskanov@gmail.com>
2023-12-16 16:51:48 +0300

    Add getMe request

---
+++ src/services/auth/auth.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+import { User } from "./types";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseURL: import.meta.env.VITE_BASE_API_URL,
+    credentials: "include",
+  }),
+  tagTypes: ["Me"],
+  endpoints: (builder) => ({
+    getMe: builder.query<User | null, void>({
+      query: () => "auth/me",
+      extraOptions: { maxRetries: false },
+      providesTags: ["Me"],
+    }),
+  }),
+});
