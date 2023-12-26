476060be80db1447106fd8e0f43b4496eccb1f75
Paskinho <v.paskanov@gmail.com>
2023-12-26 11:03:19 +0300

    Some refactoring in sign-in component

--- src/components/auth/sing-in/sign-in.stories.tsx
+++ src/components/auth/sing-in/sign-in.stories.tsx
@@ -1,18 +1,1 @@
-import { z } from "zod";
-
 export default {};
-const schema = z.object({
-  email: z.string().email("Invalid email address").nonempty("Enter email"),
-  password: z.string().nonempty("Enter password"),
-  rememberMe: z.boolean().optional(),
-});
-
-type FormType = z.infer<typeof schema>;
-
-type SignInProps = {
-  onSubmit: (data: FormType) => void;
-};
-
-export const SignIn = () => {
-  return <div>1</div>;
-};
