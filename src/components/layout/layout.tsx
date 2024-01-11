efa1e2ec3251a6a352b6503b779fa20b6cdfb182
paskanov <v.paskanov@gmail.com>
2024-01-11 12:51:13 +0300

    Add some css properties to layout and some refactoring code

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,45 +1,48 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
-import { useGetMeQuery, useLogoutMutation } from "../../services/auth/auth.ts";
+import {useGetMeQuery, useLogoutMutation} from "../../services/auth/auth.ts";
 
-import { Header } from "./header/heade.tsx";
+import {Header} from "./header/header.tsx";
 import {Spinner} from "../ui/spinner";
+import s from './layout.module.scss'
+
 
 export const Layout = () => {
-  const { data, isError, isLoading } = useGetMeQuery();
-  // const [logout] = useLogoutMutation();
-  const isAuthenticated = !isError && !isLoading;
+    const {data, isError, isLoading} = useGetMeQuery();
+    // const [logout] = useLogoutMutation();
+    const isAuthenticated = !isError && !isLoading;
 
-  if (isLoading) {
-    return <div>Is Loading</div>;
-    return <Spinner fullScreen />;
-  }
+    if (isLoading) {
+        return <div>Is Loading</div>;
+        return <Spinner fullScreen/>;
+    }
 
-  return (
-    <LayoutPrimitive
-        avatar={data?.avatar ?? null}
-        email={data?.email ?? ""}
-        isLoggedIn={isAuthenticated}
-        onLogout={()=> {}}
-        userName={data?.name ?? ""}
-    >
-      <Outlet context={{isAuthenticated} satisfies AuthContext} />
-    </LayoutPrimitive>
-  );
+    return (
+        <LayoutPrimitive
+            avatar={data?.avatar ?? null}
+            email={data?.email ?? ""}
+            isLoggedIn={isAuthenticated}
+            onLogout={() => {
+            }}
+            userName={data?.name ?? ""}
+        >
+            <Outlet context={{isAuthenticated} satisfies AuthContext}/>
+        </LayoutPrimitive>
+    );
 };
 
 export type LayoutPrimitiveProps = { children: ReactNode } & HeaderProps;
 
 export const LayoutPrimitive = ({
-  children,
-  ...headerProps
-}: LayoutPrimitiveProps) => {
-  return (
-    <div className={s.layout}>
-      <Header {...headerProps} />
-      <div className={s.content}>{children}</div>
-    </div>
-  );
+                                    children,
+                                    ...headerProps
+                                }: LayoutPrimitiveProps) => {
+    return (
+        <div className={s.layout}>
+            <Header {...headerProps} />
+            <div className={s.content}>{children}</div>
+        </div>
+    );
 };
