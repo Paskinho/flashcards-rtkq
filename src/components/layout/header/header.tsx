d1bed7f77342d6eddf10e4766790089f65b5932e
paskanov <v.paskanov@gmail.com>
2024-01-11 16:52:00 +0300

    Create logo icon and some refactoring code

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,17 +1,27 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import s from './header.module.scss'
+
 
 export type HeaderProps = {
-
+    isAuth: boolean;
+    userInfo?: {
+        email: string,
+        avatar: string;
+        name: string
+    } | null
+    onSignOut?: () => void
 }
 
-export const Header = () => {
-  return (
-      <header>
-        <div>
-          <Link to={'/'}>
+export const Header = ({isAuth, userInfo, onSignOut}: HeaderProps) => {
+    const navigate = useNavigate()
 
-          </Link>
-        </div>
-      </header>
-  );
+    return (
+        <header className={s.header}>
+            <div>
+                <Link to={'/'}>
+        <Logo/>
+                </Link>
+            </div>
+        </header>
+    );
 };
