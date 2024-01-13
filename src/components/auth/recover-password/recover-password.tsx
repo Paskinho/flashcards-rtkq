9f6afa9d43473f7d1656fd1eef8570dd6642f1d4
Paskinho <v.paskanov@gmail.com>
2024-01-13 16:03:30 +0300

    Add recover-password to all app

--- src/components/auth/recover-password/recover-password.tsx
+++ src/components/auth/recover-password/recover-password.tsx
@@ -1,1 +1,17 @@
-export const RecoverPassword = () => {};
+import { z } from "zod";
+
+import { Card } from "../../ui/card";
+
+const schema = z.object({
+  email: z.string().email("Invalid email address").nonempty("Enter email"),
+});
+
+type FormValues = z.infer<typeof schema>;
+
+type Props = {
+  onSubmit: (data: FormValues) => void;
+};
+
+export const RecoverPassword = () => {
+  return <Card></Card>;
+};
