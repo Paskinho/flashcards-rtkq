145e67494cdb809ef6d4485398cd2d6bfa16268d
paskanov <v.paskanov@gmail.com>
2024-01-15 12:26:17 +0300

    Add schema for sign-up

--- src/components/auth/sign-up/sign-up.tsx
+++ src/components/auth/sign-up/sign-up.tsx
@@ -1,17 +1,35 @@
 import s from './sign-up.module.scss'
+import { DevTool } from "@hookform/devtools";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+
+
+const schema = z.object({
+        email: z.string().email("Invalid email address").nonempty("Enter email"),
+        password: z.string().nonempty("Enter password"),
+        passwordConfirmation: z.string().nonempty("Confirm your password")
+})
+    .superRefine((data, ctx)=> {
+            if (data.password !== data.passwordConfirmation) {
+                    ctx.addIssue({
+                            message: 'Password don`t match',
+                            code: z.zodIssueCode.custom,
+                            path: ['passwordConfirmation']
+                    })
+            }
+
+    })
+
 
 export const SignUp = () => {
+
+     const {control, handleSubmit} = useForm({
+
+     })
+
 return (
-    <div>
-        1123
-        124124
-        124
-        124
-        124
-        124
-        124
-        124
-        124
-    </div>
+    <>
+    <DevTool control={control}/>
+    </>
 )
 }
