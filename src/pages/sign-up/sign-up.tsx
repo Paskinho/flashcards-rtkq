d2807dd677e3f87973ecab33699ac88e8927e98d
paskanov <v.paskanov@gmail.com>
2024-01-15 11:51:19 +0300

    Some refactoring sign-up page and create sign-up component

--- src/pages/sign-up/sign-up.tsx
+++ src/pages/sign-up/sign-up.tsx
@@ -1,15 +1,27 @@
-import {useSignUpMutation} from "../../services/auth/auth";
 import { useNavigate } from "react-router-dom";
-import {LoginArgs} from "../../services/auth/types";
+import { toast } from "react-toastify";
+
+import { useSignUpMutation } from "../../services/auth/auth";
+import { LoginArgs } from "../../services/auth/types";
+import {Page} from "../../components/ui/page";
 
 export const signUpPage = () => {
-    const [registration] = useSignUpMutation()
-    const navigate = useNavigate()
-    const handleRegistration = async (data: LoginArgs) => {
-       return registration(data)
-           .unwrap()
-           .then(()=> navigate('/'))
-           .catch(err => toast.error(err.data.message))
-    }
+  const [registration] = useSignUpMutation();
+  const navigate = useNavigate();
+  const handleRegistration = async (data: LoginArgs) => {
+    return registration(data)
+      .unwrap()
+      .then(() => {
+        toast.success("Signed up successfully");
+        navigate("/");
+      })
+      .catch((err) => toast.error(err.data.message));
+  };
 
-}
+  return (
+      <Page flex>
+          <SignUp onSubmit={handleRegistration}/>
+      </Page>
+  )
+
+};
