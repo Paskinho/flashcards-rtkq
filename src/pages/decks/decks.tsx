b2afdb903d69a0bf7c5d00e484d9182b3604a275
paskanov <v.paskanov@gmail.com>
2024-01-17 12:13:47 +0300

    Some refactoring in decks scss

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -3,6 +3,9 @@ import {z} from "zod";
 import {Page} from "../../components/ui/page";
 import {useForm} from "react-hook-form";
 import {Typography} from "../../components/ui/typography";
+import {Modal} from "../../components/ui/modal";
+import {Button} from "../../components/ui/button";
+import s from './decks.module.scss'
 
 
 const schema = z.object({})
@@ -18,9 +21,15 @@ export const Decks = ({onSubmit}: DecksProps) => {
 
 
     return (
-        <>
             <Page >
-                <Typography variant={'large'}>Decks list</Typography>            </Page>
-        </>
+                <div className={s.page}>
+                <Typography variant={'large'}>Decks list</Typography>
+                <Button>Add New Pack</Button>
+                <Modal>
+
+                </Modal>
+                </div>
+            </Page>
+
     )
 }
