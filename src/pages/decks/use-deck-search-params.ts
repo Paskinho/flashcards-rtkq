e234c8eadcb1d27f262d35b9ced7780b6704f0f0
paskanov <v.paskanov@gmail.com>
2024-01-25 16:58:18 +0300

    Create hook use-query-param

--- src/pages/decks/use-deck-search-params.ts
+++ src/pages/decks/use-deck-search-params.ts
@@ -3,4 +3,6 @@ import {useSearchParams} from "react-router-dom";
 
 const useDeckSearchParams = () => {
    const [searchParams, setSearchParams] = useSearchParams()
+   const [currentPage, setCurrentPage] = 1
+
 }
