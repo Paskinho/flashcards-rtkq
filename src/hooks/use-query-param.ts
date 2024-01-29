d6fc06ff438852bd1920962b648014fcdacf09e4
paskanov <v.paskanov@gmail.com>
2024-01-29 15:13:45 +0300

    Refactoring hooks use query param

--- src/hooks/use-query-param.ts
+++ src/hooks/use-query-param.ts
@@ -1,15 +1,24 @@
+import { isNil } from 'remeda'
 
-
-export function useQueryParam<T extends boolean | number | string> (
-  searchParams: URLSearchParams,
-  setSearchParams: (searchParams: URLSearchParams) => void,
-  param: string,
-  defaultValue?:T
-) : [T | null, (value: T | null) => void] {
+export function useQueryParam<T extends boolean | number | string>(
+    searchParams: URLSearchParams,
+    setSearchParams: (searchParams: URLSearchParams) => void,
+    param: string,
+    defaultValue?: T
+): [T | null, (value: T | null) => void] {
     const paramValue = searchParams.get(param)
     const convertedValue = getConvertedValue<T>(paramValue, defaultValue)
 }
 
-const getConvertedValue () => {
-
+function getConvertedValue<T>(value: null | string, defaultValue: T | undefined): T | null {
+    if (value === null) {
+        return defaultValue ?? null
+    }
+    if (value === 'true' || value === 'false') {
+        return (value = 'true') as unknown as T
+    }
+    if (!isNaN(Number(value))) {
+        return Number(value) as unknown as T
+    }
+    return value as unknown as T
 }
