1d788d1aa48ab6a48fb16d9ad6c02ab5800b16f1
paskanov <v.paskanov@gmail.com>
2024-02-07 12:46:52 +0300

    Some refactoring in all app

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import "./styles/index.scss";
 
-import { StrictMode } from "react";
+import {StrictMode} from "react";
 
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { createBrowserRouter } from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
 
-import { store } from "../src/app/store";
-import { LoginForm } from "../src/components/auth/login-form/login-form";
+import {store} from "../src/app/store";
 
-import { App } from "./app/App";
+import {App} from "./app/App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
