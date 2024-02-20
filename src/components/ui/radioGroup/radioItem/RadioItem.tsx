1f76455fa086d6bfc7c0de3385a1326a7ac18978
paskanov <v.paskanov@gmail.com>
2024-02-20 11:23:12 +0300

    Some refactoring radioItem component

--- src/components/ui/radioGroup/radioItem/RadioItem.tsx
+++ src/components/ui/radioGroup/radioItem/RadioItem.tsx
@@ -1,4 +1,4 @@
-import {ComponentPropsWithoutRef} from "react";
+import {ComponentPropsWithoutRef, ElementRef, forwardRef, JSX } from "react";
 import {Radio} from "@radix-ui/react-radio-group";
 
 
@@ -6,6 +6,8 @@ export type RadioItemProps = {
     title: string
 } & ComponentPropsWithoutRef<typeof Radio.Item>
 
-export const RadioItem = () => {
+export const RadioItem = forwardRef<ElementRef<typeof Radio.Item>, RadioItemProps>(
+    ({value, title, disabled}, ref): JSX.Element
+) => {
 
 }
