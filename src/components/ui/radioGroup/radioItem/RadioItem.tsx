54c7b1050d03ffc5b73d10b32633f284190d3c31
paskanov <v.paskanov@gmail.com>
2024-02-20 11:56:07 +0300

    Some refactoring radioItem component and some css

--- src/components/ui/radioGroup/radioItem/RadioItem.tsx
+++ src/components/ui/radioGroup/radioItem/RadioItem.tsx
@@ -1,13 +1,16 @@
-import {ComponentPropsWithoutRef, ElementRef, forwardRef, JSX } from "react";
+import {ComponentPropsWithoutRef, ElementRef, forwardRef, JSX} from "react";
 import {Radio} from "@radix-ui/react-radio-group";
-
+import cn from 'classnames'
+import s from './RadioItem.module.scss'
 
 export type RadioItemProps = {
     title: string
 } & ComponentPropsWithoutRef<typeof Radio.Item>
 
 export const RadioItem = forwardRef<ElementRef<typeof Radio.Item>, RadioItemProps>(
-    ({value, title, disabled}, ref): JSX.Element
-) => {
-
-}
+    ({value, title, disabled}, ref): JSX.Element => {
+     const classNames = {
+         label: cn(s.label, disabled && s.disabled)
+     }
+    }
+)
