b6551ba1047a80822d9b5943065fb9b7bab1167d
paskanov <v.paskanov@gmail.com>
2024-02-20 16:37:58 +0300

    Add mixins and some refactoring in radio components

--- src/pages/learnPage/rateLearnCard/RateLearnCard.tsx
+++ src/pages/learnPage/rateLearnCard/RateLearnCard.tsx
@@ -1,8 +1,17 @@
 import {Button} from "../../../components/ui/button";
+import {ControlledRadioGroup} from "../../../components/controlled/controlled-radioGroup";
+import s from './RateLearnCard.module.scss'
+
+export type RateLearnCardProps = {
+    grade: string
+}
 
 export const RateLearnCard = () => {
     return (
         <form>
+            <ControlledRadioGroup
+                className={s.radioGroup}
+            />
             <Button type={'submit'} fullWidth>
                 Next Question
             </Button>
