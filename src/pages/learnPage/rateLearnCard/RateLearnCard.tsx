667950a043b843b14892e2a62eff64a8618cbc84
paskanov <v.paskanov@gmail.com>
2024-02-20 16:44:43 +0300

    Finish RateLearnCard.tsx

--- src/pages/learnPage/rateLearnCard/RateLearnCard.tsx
+++ src/pages/learnPage/rateLearnCard/RateLearnCard.tsx
@@ -1,21 +1,35 @@
 import {Button} from "../../../components/ui/button";
 import {ControlledRadioGroup} from "../../../components/controlled/controlled-radioGroup";
 import s from './RateLearnCard.module.scss'
+import {JSX} from "react";
+import {useForm} from "react-hook-form";
+import {rateOptions} from "./rateOptions";
 
-export type RateLearnCardProps = {
+export type RateLearnCardValues = {
     grade: string
 }
 
-export const RateLearnCard = () => {
+type Props = {
+    onSubmit: (data: RateLearnCardValues) => void
+}
+
+export const RateLearnCard = ({onSubmit}: Props): JSX.Element => {
+
+    const {control, handleSubmit} = useForm<RateLearnCardValues>({
+        defaultValues:{grade: '1'}
+    })
+
     return (
         <form>
             <ControlledRadioGroup
                 className={s.radioGroup}
+                control={control}
+                name={'grade'}
+                options={rateOptions}
             />
             <Button type={'submit'} fullWidth>
                 Next Question
             </Button>
-
         </form>
     )
 }
