b89d2558b8abba1f96e53d866a1b995527faf46a
paskanov <v.paskanov@gmail.com>
2024-02-21 12:51:51 +0300

    Some refactoring in ControlledRadioGroup.tsx

--- src/components/controlled/controlled-radioGroup/ControlledRadioGroup.tsx
+++ src/components/controlled/controlled-radioGroup/ControlledRadioGroup.tsx
@@ -1,21 +1,20 @@
 import {RadioGroup, RadioGroupProps} from "../../ui/radioGroup";
 import {FieldValues, useController, UseControllerProps} from "react-hook-form";
-import { JSX } from 'react'
+import {JSX} from 'react'
 
 type ControlledRadioGroupProps<T extends FieldValues> = UseControllerProps<T> &
     Omit<RadioGroupProps, 'value | onValueChange'>
 
 export const ControlledRadioGroup = <T extends FieldValues>({
-    name,
-    control,
-    ...restProps
-                                                            } :ControlledRadioGroupProps<T>): JSX.Element =>
-{
+                                                                name,
+                                                                control,
+                                                                ...restProps
+                                                            }: ControlledRadioGroupProps<T>): JSX.Element => {
     const {field: {value, onChange},} = useController({
         name,
         control
     })
-  return (
-      <RadioGroup {...restProps} onValueChange={onChange} value={value} name={name}/>
-  )
+    return (
+        <RadioGroup {...restProps} onValueChange={onChange} value={value} name={name}/>
+    )
 }
